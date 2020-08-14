c08fcb2c803ef80ea7fe4b9c7fe2a946c3b6accb
wan-nyan-wan <distributed.system.love@gmail.com>
2020-08-14 14:41:57 +0900

    fix desc

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div``;
-const Title = styled.h1``;
+const Title = styled.h1`
+  text-align: center;
+`;
+const Description = styled.p``;
 
 export function Header(): JSX.Element {
   return (
     <Container>
       <Title>My name is hnkz.</Title>
+      <Description>
+        Repository is{" "}
+        <a href="https://github.com/wan-nyan-wan/wan-nyan-wan.github.io/">
+          here
+        </a>
+      </Description>
     </Container>
   );
 }
