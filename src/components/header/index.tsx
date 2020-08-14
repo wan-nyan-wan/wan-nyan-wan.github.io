95f91b98da46b30c1594e997ccbc8b0ecae25d11
wan-nyan-wan <distributed.system.love@gmail.com>
2020-08-14 14:27:29 +0900

    fix to use esbuild

---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h1``;
+
+export function Header(): JSX.Element {
+  return (
+    <Container>
+      <Title>My name is hnkz.</Title>
+    </Container>
+  );
+}
